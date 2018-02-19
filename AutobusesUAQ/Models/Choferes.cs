a503f69fa71a33aaefb273f467db12fedbb6ea32
antonioantunez7 <scorpion_malboro@hotmail.com>
2018-02-19 12:22:39 -0600

    m

---
+++ AutobusesUAQ/Models/Choferes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace AutobusesUAQ.Models
+{
+    public class Choferes
+    {
+        public Choferes() { }
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apaterno { get; set; }
+        public string amaterno { get; set; }
+        public string email { get; set; }
+        public string rfc { get; set; }
+        public string calle { get; set; }
+        public string colornia { get; set; }
+        public string cp { get; set; }
+        public string exterior { get; set; }
+        public string interior { get; set; }
+        public string telCelular { get; set; }
+        public string telCasa { get; set; }
+    }
+}
